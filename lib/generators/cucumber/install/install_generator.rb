e9ea14fd55f0f7a8f30066d6b18f613cbb83a6a7
Steve Tooke <steve@boxjump.co.uk>
2015-11-05 18:01:09 +0000

    Simple install generator

--- lib/generators/cucumber/install/install_generator.rb
+++ lib/generators/cucumber/install/install_generator.rb
@@ -1,6 +1,12 @@
 module Cucumber
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      def copy_rails_support
+        copy_file "rails.rb", "features/support/rails.rb"
+      end
+
     end
   end
 end
