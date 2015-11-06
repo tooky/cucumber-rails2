6e850fb41ba3f9e657f4e047e14a8cf935d60b5d
Steve Tooke <steve@boxjump.co.uk>
2015-11-06 10:04:53 +0000

    Capybara doesn't need to be a requirement.
    Add a convenience stub indicating that Capybara should be installed.
    Assumes that `visit` will be first Capybara dsl method called.

---
+++ lib/cucumber/rails.rb
@@ -0,0 +1,15 @@
+module Cucumber
+  module Rails
+    module World
+      def visit(*)
+        if defined?(super)
+          super
+        else
+          raise Cucumber::Pending, "Capybara not loaded, please add it to your Gemfile:\n\ngem \"capybara\"\n\n"
+        end
+      end
+    end
+  end
+end
+
+World(Cucumber::Rails::World)
--- lib/generators/cucumber/install/templates/rails.rb
+++ lib/generators/cucumber/install/templates/rails.rb
@@ -6,6 +6,4 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../../config/environment', __FILE__)
 require 'cucumber/rails'
-require 'capybara/rails'
-require 'capybara/cucumber'
 
