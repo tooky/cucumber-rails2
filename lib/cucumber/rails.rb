b82da8647d5b3d3bdd5e25ad81495b340689b250
Steve Tooke <steve@boxjump.co.uk>
2015-11-09 17:06:53 +0000

    Move allow rescue code to its own file.

--- lib/cucumber/rails.rb
+++ lib/cucumber/rails.rb
@@ -1,23 +1,2 @@
 require 'cucumber/rails/capybara'
-
-ActionController::Base.class_eval do
-  cattr_accessor :allow_rescue
-end
-
-ActionDispatch::ShowExceptions.class_eval do
-  alias __cucumber_orig_call__ call
-
-  def call(env)
-    env['action_dispatch.show_exceptions'] = !!ActionController::Base.allow_rescue
-    __cucumber_orig_call__(env)
-  end
-end
-
-Before('@allow-rescue') do
-  @__orig_allow_rescue = ActionController::Base.allow_rescue
-  ActionController::Base.allow_rescue = true
-end
-
-After('@allow-rescue') do
-  ActionController::Base.allow_rescue = @__orig_allow_rescue
-end
+require 'cucumber/rails/allow_rescue'
