84024fe125b64ac475b9cfc0987c75f85bd7a480
Steve Tooke <steve@boxjump.co.uk>
2015-11-19 16:52:06 +0000

    Move RackTest into its own file

--- lib/cucumber/rails.rb
+++ lib/cucumber/rails.rb
@@ -1,25 +1,7 @@
 require 'cucumber/rails/allow_rescue'
 require 'cucumber/rails/capybara'
 require 'cucumber/rails/database'
-
-require 'rack/test'
-module Cucumber
-  module Rails
-    module RackTest
-      include Rack::Test::Methods
-
-      def app
-        Rack::Builder.new do
-          map "/" do
-            run ::Rails.application
-          end
-        end.to_app
-      end
-    end
-  end
-end
-
-World(Cucumber::Rails::RackTest)
+require 'cucumber/rails/rack_test'
 
 begin
   require 'database_cleaner'
---
+++ lib/cucumber/rails/rack_test.rb
@@ -0,0 +1,20 @@
+require 'rack/test'
+module Cucumber
+  module Rails
+    module RackTest
+      include Rack::Test::Methods
+
+      def app
+        Rack::Builder.new do
+          map "/" do
+            run ::Rails.application
+          end
+        end.to_app
+      end
+    end
+  end
+end
+
+World(Cucumber::Rails::RackTest)
+
+
